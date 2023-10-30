20efd81e29d91ff04e9696dec5616c6616aedf6d
Alin Iordache <iordache.alin@yahoo.com>
2023-10-30 17:54:49 +0200

    Added article about Algovis

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  site: config.site.base_url ? config.site.base_url : "https://frugalcode.vercel.app",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   integrations: [
@@ -23,7 +23,7 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [],
     shikiConfig: {
-      theme: "one-dark-pro",
+      theme: "material-theme-palenight",
       wrap: true,
     },
     extendDefaultPlugins: true,
